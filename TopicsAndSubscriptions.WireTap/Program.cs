19f7e61e19852b5b7a8a3d7c87d4ad1f1ab016f7
Piotr Ludwiczuk <plu@schultz.dk>
2022-07-20 17:27:15 +0200

    wiretap pattern

---
+++ TopicsAndSubscriptions.WireTap/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+using Azure.Messaging.ServiceBus;
+using Azure.Messaging.ServiceBus.Administration;
+using WorkingWithMessages.Config;
+using static System.Console;
+
+string ServiceBusConnectionString = Settings.GetConnectionString();
+string TopicName = "Orders";
+
+WriteLine("Wire Tap Console");
+WriteLine("Press enter to activate wire tap");
+ReadLine();
+
+var subscriptionName = $"wiretap-{ Guid.NewGuid() }";
+
+var managementClient = new ServiceBusAdministrationClient(ServiceBusConnectionString);
+
+var options = new CreateSubscriptionOptions(TopicName, subscriptionName)
+{
+    AutoDeleteOnIdle = TimeSpan.FromMinutes(5)
+};
+var subscription = await managementClient.CreateSubscriptionAsync(options);
+
+await using var serviceBusClient = new ServiceBusClient(ServiceBusConnectionString);
+var receiver = serviceBusClient.CreateReceiver(TopicName, subscriptionName);
+
+WriteLine($"Receiving on { subscriptionName }");
+WriteLine("Press enter to quit...");
+
+while(true)
+{ 
+    var message = await receiver.ReceiveMessageAsync();
+    if(message != null)
+    {
+        InspectMessage(message);
+        await receiver.CompleteMessageAsync(message);
+    }
+}
+
+await receiver.CloseAsync();
+
+void InspectMessage(ServiceBusReceivedMessage message)
+{
+    WriteLine($"Received message...");
+
+    WriteLine("Properties");
+    WriteLine($"    ContentType             - { message.ContentType }");
+    WriteLine($"    CorrelationId           - { message.CorrelationId }");
+    WriteLine($"    Subject                 - { message.Subject }");
+    WriteLine($"    MessageId               - { message.MessageId }");
+    WriteLine($"    PartitionKey            - { message.PartitionKey }");
+    WriteLine($"    ReplyTo                 - { message.ReplyTo }");
+    WriteLine($"    ReplyToSessionId        - { message.ReplyToSessionId }");
+    WriteLine($"    ScheduledEnqueueTime    - { message.ScheduledEnqueueTime }");
+    WriteLine($"    SessionId               - { message.SessionId }");
+    WriteLine($"    TimeToLive              - { message.TimeToLive }");
+    WriteLine($"    To                      - { message.To }");
+
+    WriteLine("ApplicationProperties");
+    foreach (var property in message.ApplicationProperties)
+    {
+        WriteLine($"    { property.Key } - { property.Value }");
+    }
+
+    WriteLine("Body");
+    WriteLine($"{ Encoding.UTF8.GetString(message.Body) }");
+    WriteLine();
+}
