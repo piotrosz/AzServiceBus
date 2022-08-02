c4ca3a57a54a7fe6e588e307507d44389411694b
Piotr Ludwiczuk <plu@schultz.dk>
2022-08-02 15:56:02 +0200

    Add ErrorHangling

---
+++ ErrorHandling/DeadLetterReceiver/Program.cs
@@ -0,0 +1,51 @@
+﻿using Azure.Messaging.ServiceBus;
+using CommonServiceBusConnectionString;
+using DeadLetterReceiver;
+
+Thread.Sleep(3000);
+
+var queueClient = new ServiceBusClient(Settings.GetConnectionString());
+string queueName = "errorhandling";
+
+Utils.WriteLine("DeadLetterReceiverConsole", ConsoleColor.White);
+Console.WriteLine();
+
+var options = new ServiceBusProcessorOptions
+{
+    MaxConcurrentCalls = 1,
+    AutoCompleteMessages = false,
+    SubQueue = SubQueue.DeadLetter
+};
+
+var processor =  queueClient.CreateProcessor(queueName, options);
+
+Utils.WriteLine($"Dead letter path: {processor.EntityPath}", ConsoleColor.Cyan);
+
+processor.ProcessMessageAsync += ProcessDeadLetterMessageAsync;
+processor.ProcessErrorAsync += ProcessErrorAsync;
+
+await processor.StopProcessingAsync();
+
+Utils.WriteLine("Receiving dead letter messages", ConsoleColor.Cyan);
+Console.WriteLine();
+
+Console.ReadLine();
+await processor.StopProcessingAsync();
+await processor.CloseAsync();
+
+async Task ProcessDeadLetterMessageAsync(ProcessMessageEventArgs message)
+{
+    Utils.WriteLine("Received dead letter message", ConsoleColor.Cyan);
+    Utils.WriteLine($"    Content type: { message.Message.ContentType }", ConsoleColor.Green);
+    Utils.WriteLine($"    DeadLetterReason: { message.Message.DeadLetterReason }", ConsoleColor.Green);
+    Utils.WriteLine($"    DeadLetterErrorDescription: { message.Message.DeadLetterErrorDescription }", ConsoleColor.Green);
+
+    await message.CompleteMessageAsync(message.Message);
+    
+    Console.WriteLine();
+}
+
+async Task ProcessErrorAsync(ProcessErrorEventArgs arg)
+{
+    Utils.WriteLine($"Exception: { arg.Exception.Message }", ConsoleColor.Yellow);
+}
