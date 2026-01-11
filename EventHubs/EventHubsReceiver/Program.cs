5440998f623ac75e86d0bb613f91fc2fd7b8a1da
Piotr Ludwiczuk <piotrosz@gmail.com>
2026-01-11 18:49:38 +0100

    evhubs

---
+++ EventHubs/EventHubsReceiver/Program.cs
@@ -0,0 +1,64 @@
+﻿using Azure.Identity;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Consumer;
+using Azure.Messaging.EventHubs.Processor;
+using Azure.Storage.Blobs;
+using System.Text;
+
+const string blobContainerUri = "https://sa0piotr0eventhubs.blob.core.windows.net/blobcontainer";
+// Create a blob container client that the event processor will use
+var storageClient = new BlobContainerClient(
+    new Uri(blobContainerUri),
+    new DefaultAzureCredential());
+
+// Create an event processor client to process events in the event hub
+const string qualifiedNamespace = "";
+const string eventHubName = "";
+
+var processor = new EventProcessorClient(
+    storageClient,
+    EventHubConsumerClient.DefaultConsumerGroupName,
+    qualifiedNamespace, 
+    eventHubName,
+    new DefaultAzureCredential());
+
+// Register handlers for processing events and handling errors
+processor.ProcessEventAsync += ProcessEventHandler;
+processor.ProcessErrorAsync += ProcessErrorHandler;
+
+await processor.StartProcessingAsync();
+
+// Wait for 'x' key to be pressed to stop processing
+Console.WriteLine("Press 'x' to stop processing events...");
+while (true)
+{
+    if (Console.KeyAvailable)
+    {
+        var key = Console.ReadKey(intercept: true);
+        if (key.KeyChar is 'x' or 'X')
+        {
+            break;
+        }
+    }
+    await Task.Delay(100); // Small delay to prevent CPU spinning
+}
+
+await processor.StopProcessingAsync();
+
+return;
+
+Task ProcessEventHandler(ProcessEventArgs eventArgs)
+{
+    var eventBody = Encoding.UTF8.GetString(eventArgs.Data.Body.ToArray());
+    // Write the body of the event to the console window
+    Console.WriteLine("Received event: {0}", eventBody);
+    return Task.CompletedTask;
+}
+
+Task ProcessErrorHandler(ProcessErrorEventArgs eventArgs)
+{
+    // Write details about the error to the console window
+    Console.WriteLine($"\tPartition '{eventArgs.PartitionId}': an unhandled exception was encountered. This was not expected to happen.");
+    Console.WriteLine(eventArgs.Exception.Message);
+    return Task.CompletedTask;
+}
---
+++ EventHubs/EventHubsSender/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Azure.Identity;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+using System.Text;
+
+// number of events to be sent to the event hub
+var numOfEvents = 4;
+
+// The Event Hubs client types are safe to cache and use as a singleton for the lifetime
+// of the application, which is best practice when events are being published or read regularly.
+
+const string namespaceName = "";
+const string eventHubName = "";
+
+await using var producerClient = new EventHubProducerClient(
+    namespaceName,
+    eventHubName,
+    new DefaultAzureCredential());
+
+// Create a batch of events 
+using var eventBatch = await producerClient.CreateBatchAsync();
+
+for (var i = 1; i <= numOfEvents; i++)
+{
+    if (!eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes($"Test Event {i}"))))
+    {
+        // if it is too large for the batch
+        throw new Exception($"Event {i} is too large for the batch and cannot be sent.");
+    }
+}
+
+// Use the producer client to send the batch of events to the event hub
+await producerClient.SendAsync(eventBatch);
+Console.WriteLine($"A batch of {numOfEvents} events has been published.");
+Console.ReadLine();
